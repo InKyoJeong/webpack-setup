7530b45e28a37dcee8dd2a864383fc36f1a7514e
InKyoJeong <viapolar@naver.com>
2022-07-10 19:59:53 +0900

    chore: add output option (clean) - set true

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,68 +1,71 @@
-import path from "path";
-import webpack from "webpack";
-import "webpack-dev-server";
-import HtmlWebpackPlugin from "html-webpack-plugin";
+import path from 'path';
+import webpack from 'webpack';
+import 'webpack-dev-server';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
-const isDevelopment = process.env.NODE_ENV !== "production";
+const isDevelopment = process.env.NODE_ENV !== 'production';
 
 const config: webpack.Configuration = {
-  mode: isDevelopment ? "development" : "production",
+  mode: isDevelopment ? 'development' : 'production',
   entry: {
-    app: "./src/index.tsx",
+    app: './src/index.tsx',
   },
   resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
     alias: {
-      "@hooks": path.resolve(__dirname, "src/hooks"),
-      "@components": path.resolve(__dirname, "src/components"),
-      "@pages": path.resolve(__dirname, "src/pages"),
-      "@typings": path.resolve(__dirname, "src/typings"),
-      "@constants": path.resolve(__dirname, "src/constants"),
-      "@utils": path.resolve(__dirname, "src/utils"),
-      "@styles": path.resolve(__dirname, "src/styles"),
-      "@assets": path.resolve(__dirname, "src/assets"),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@typings': path.resolve(__dirname, 'src/typings'),
+      '@constants': path.resolve(__dirname, 'src/constants'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
     },
   },
   output: {
-    publicPath: "/",
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].bundle.js",
-    assetModuleFilename: "images/[hash][ext][query]",
+    publicPath: '/',
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+    assetModuleFilename: 'images/[hash][ext][query]',
+    clean: true,
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
         options: {
           presets: [
-            "@babel/preset-env",
-            ["@babel/preset-react", { runtime: "automatic" }],
-            "@babel/preset-typescript",
+            '@babel/preset-env',
+            ['@babel/preset-react', { runtime: 'automatic' }],
+            '@babel/preset-typescript',
           ],
         },
-        exclude: path.join(__dirname, "node_modules"),
+        exclude: path.join(__dirname, 'node_modules'),
       },
       {
         test: /\.css?$/,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.png/,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "public/index.html",
-      favicon: "public/favicon.ico",
+      template: 'public/index.html',
+      favicon: 'public/favicon.ico',
     }),
   ],
   devServer: {
     historyApiFallback: true,
     port: 3000,
-    static: { directory: path.resolve(__dirname, "public") },
+    open: true,
+    hot: true,
+    static: { directory: path.resolve(__dirname, 'public') },
   },
 };
 
